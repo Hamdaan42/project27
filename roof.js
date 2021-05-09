4ede64243a72b6394d8ca20a175b435de9d9af81
Hamdaan42 <71827478+Hamdaan42@users.noreply.github.com>
2021-05-09 21:19:41 +0530

    Add files via upload

---
+++ roof.js
@@ -0,0 +1,30 @@
+class ground
+{
+    constructor(x,y,w,h)
+    {
+        var options={
+            isStatic:true
+            
+
+        }
+        this.x=x;
+        this.y=y;
+        this.w=w;
+        this.h=h;
+        this.body=Bodies.rectangle(x,y,w,h,options)
+        World.add(world,this.body);
+
+    }
+    display()
+    {
+     var paperpos=this.body.position;
+
+     push()
+     translate(paperpos.x,paperpos.y);
+     rectMode(CENTER)
+     strokeWeight(3);
+     fill(255,0,255)
+     rect(0,0,this.w,this.h)
+     pop()   
+    }
+}
--- sketch.js
+++ sketch.js
@@ -3,6 +3,14 @@ const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
+const Constraint = Matter.Constraint;
+var bobObject1,bobObject2,bobObject3,bobObject4,bobObject5;
+var rope1,rope2,rope3,rope4,rope5;
+var roofObject;
+
+
+
+
 
 function preload()
 {
@@ -15,21 +23,18 @@ function setup() {
 
 	engine = Engine.create();
 	world = engine.world;
-
-	//Create the Bodies Here.
-	constructor(body1,body2, offsetX, offsetY)
-	{
-		this.offsetX=offsetX
-		this.offsetY=offsetY
-		var options={
-			BodyA:body1,
-			bodyB:body2,
-			pointB:{x:this.offsetX, y:this.offsetY}}
-
-		}
-		this.rope=Constructor.create(options)
-		World.add(world,this.rope)
-	}
+    roofObject=new ground(400,250,230,20)
+	bobObject1=new papper(320,575,40)
+	bobObject2=new papper(360,575,40)
+	bobObject3=new papper(400,575,40)
+	bobObject4=new papper(440,575,40)
+	bobObject5=new papper(480,575,40)
+	rope1=new rope(bobObject1.body,roofObject.body,-80, 0)
+	rope2=new rope(bobObject2.body,roofObject.body,-40, 0)
+	rope3=new rope(bobObject3.body,roofObject.body,0, 0)
+	rope4=new rope(bobObject4.body,roofObject.body,40, 0)
+	rope5=new rope(bobObject5.body,roofObject.body,80, 0)
+	
 
 
 	Engine.run(engine);
@@ -39,11 +44,36 @@ function setup() {
 
 function draw() {
   rectMode(CENTER);
-  background(0);
+  background("white");
+  roofObject.display()
+  bobObject1.display()
+  bobObject2.display()
+  bobObject3.display()
+  bobObject4.display()
+  bobObject5.display()
+  rope1.display()
+  rope2.display()
+  rope3.display()
+  rope4.display()
+  rope5.display()
+
+
+
   
   drawSprites();
  
 }
+
+
+
+function keyPressed() {
+	if (keyCode===UP_ARROW) {
+		Matter.Body.applyForce(bobObject1.body,bobObject1.body.position,{
+			x:-50,y:-145
+		})
+	}
+}
+
 
 
 
